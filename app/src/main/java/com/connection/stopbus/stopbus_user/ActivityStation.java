62eebfab6ae5e5d9275307aa88291653f52891be
Danbk <tiger8229@gmail.com>
2018-05-19 18:31:18 +0900

    -

--- app/src/main/java/com/connection/stopbus/stopbus_user/ActivityStation.java
+++ app/src/main/java/com/connection/stopbus/stopbus_user/ActivityStation.java
@@ -221,8 +221,6 @@ public class ActivityStation extends Activity{
                             }
                         }
                 );
-gi
-
 
             }catch (Exception e){
                 e.printStackTrace();
